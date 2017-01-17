9a51e78b753d8ede4aaccadaf5d81da089f7eca3
Julien <julien.linder@gmail.com>
2017-01-17 16:55:59 +0100

    une petite avancé mais pas de javascript

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-function chronoStart {
+var timer = 0;
+var counter = 0;
+$("#start").click(function(){
+	currentTime:0;
+	console.log(counter);
 
-};
-
-function chronoStop {
-
-};
-
-function chronoReset {
-
-};
+});
