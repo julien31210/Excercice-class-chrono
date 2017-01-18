3f0b81ddeaef6d30191596c911ce5905e7367bd1
Julien <julien.linder@gmail.com>
2017-01-18 11:00:27 +0100

    ajout des secondes minutes et heures eureka ça marche

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var sec = 0;
+var min = 0;
+var h = 0;
 $("#start").click(function(){
 	inter = setInterval(function(){
 		sec++
+		$("#sec").html(sec);
 		console.log(sec);
+		
+		if (sec==60) {
+			min++;
+			sec=0;
+		}
+		$("#sec").html(sec);
+		$("#min").html(min);
+
+		if (min==60) {
+			h++;
+			min=0;
+		}
+
+		$("#min").html(min);
+		$("#h").html(h);
 	}, 1000)
+
+
 });
